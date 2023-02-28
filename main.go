baab413bd7bb6695721e8bcd887abcf468da2d41
Master Mara <mustafa.karacabey.engineerx@gmail.com>
2023-02-28 17:54:42 +0300

    [Some Updates for ReStudy]

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+const golangBirthDate = 2007
+
+type golangStruct struct {
+	age int
+}
+
+func calculateGolangAge(time time.Time) int {
+	return time.Year() - golangBirthDate
+}
+
+func (g *golangStruct) destroyStruct() {
+	fmt.Println("Destroy Struct...")
+	g = nil
+}
+
 func main() {
 
+	myStr := golangStruct{
+		age: calculateGolangAge(time.Now()),
+	}
+
+	fmt.Printf("beforeDestroy myStr address:%p\t myStr value:%v\n", &myStr, myStr.age)
+
+	myStr.destroyStruct()
+
+	fmt.Printf("afterDestroy myStr address:%p\t myStr value:%v\n", &myStr, myStr.age)
 }
